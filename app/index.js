c8ff06865cbbc540e7720a2f12c18c671762b35e
aluis92 <andre_luis9214@hotmail.com>
2016-03-26 10:00:34 -0300

    basic structure

---
+++ app/index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var generators = require("yeoman-generator");
+
+module.exports = generators.Base.extend({
+    constructor: function() {
+        // Calling the super constructor is important so our generator is correctly set up
+        generators.Base.apply(this, arguments);
+
+        console.log('hello');
+    },
+    initializing: function() {
+    },
+    prompting: function() {
+    },
+    configuring: function() {
+    },
+    default: function() {
+    },
+    writing: {
+        bower: function() {
+            var bowerJson = {
+                name: 'Nanoscope',
+                license: 'MIT',
+                dependencies: {}
+
+            };
+            bowerJson.dependencies['jquery'] = '~1.12.0';
+            this.fs.writeJSON('bower.json', bowerJson);
+        },
+        statics: function() {
+            //this.copy("_test.txt", "src/test.txt");
+            // inside assets
+            this.directory("assets/fonts", "src/assets/fonts");
+            this.directory("assets/img", "src/assets/img");
+            this.directory("assets/scripts", "src/assets/scripts");
+            this.directory("assets/styles", "src/assets/styles");
+            this.directory("assets/vendor", "src/assets/vendor");
+
+            //modules
+            this.directory("_modules", "src/_modules");
+        },
+        scripts: function() {
+
+        },
+        jade: function() {
+            this.fs.copyTpl(
+                this.templatePath("_layout.jade"),
+                this.destinationPath("src/_layout.jade"),
+                {
+                    appname: "Nanoscope"
+                }
+            );
+        }
+    },
+    conflicts: function() {
+    },
+    install: function() {
+    },
+    end: function() {
+    }
+});
