79f79092dde21a309a900d2e8f3450a6e7d5ebb5
aluis92 <andre_luis9214@hotmail.com>
2016-03-27 13:54:04 -0300

    libs options and naming the app

--- app/index.js
+++ app/index.js
@@ -1,17 +1,64 @@
 'use strict';
 
-var generators = require("yeoman-generator");
+var generators = require("yeoman-generator"),
+    _ = require("lodash"),
+    chalk = require("chalk"),
+    yosay = require("yosay");
 
 module.exports = generators.Base.extend({
     constructor: function() {
         // Calling the super constructor is important so our generator is correctly set up
         generators.Base.apply(this, arguments);
 
-        console.log('hello');
+        // this.argument("appname", {type: String, required: true});
+        // this.appname = _.kebabCase(this.appname)
+
+        // this.option("includeJquery", {
+        //     desc: 'includes Jquery',
+        //     type: Boolean,
+        //     default: false
+        // })
     },
     initializing: function() {
     },
     prompting: function() {
+        this.log(yosay("welcome to " + chalk.blue("nanoscope")));
+
+        var done = this.async();
+        this.prompt([
+            {
+                type: "input",
+                name: "appname",
+                message: "Application name",
+                default: "myApp"
+            },
+            {
+                type: "checkbox",
+                name: "jslibs",
+                message: "Select JavaScript libraries that you wanna use.",
+                choices: [
+                    {
+                        name: "Jquery",
+                        value: "jquery",
+                        checked: "true"
+                    },
+                    {
+                        name: "Umbrellajs",
+                        value: "umbrellajs",
+                        checked: "true"
+                    }
+                ]
+            }
+
+        ], function(resp) {
+            console.log(resp);
+            this.appname = resp.appname
+            done();
+
+            this.includeJquery = _.includes(resp.jslibs, "jquery");
+            this.includeUmbrellajs = _.includes(resp.jslibs, "umbrellajs");
+
+        }.bind(this));
     },
     configuring: function() {
     },
@@ -27,12 +74,20 @@ module.exports = generators.Base.extend({
         },
         bower: function() {
             var bowerJson = {
-                name: 'Nanoscope',
+                name: this.appname,
                 license: 'MIT',
                 dependencies: {}
-
             };
-            bowerJson.dependencies['jquery'] = '~1.12.0';
+
+            if(this.includeJquery) {
+                bowerJson.dependencies['jquery'] = '~1.12.0';
+            }
+
+            if(this.includeUmbrellajs) {
+                bowerJson.dependencies['umbrella'] = '^2.0.1';
+            }
+            bowerJson.dependencies['umbrella'] = '^2.0.1';
+
             this.fs.writeJSON('bower.json', bowerJson);
 
             this.copy("bowerrc", ".bowerrc")
@@ -63,7 +118,7 @@ module.exports = generators.Base.extend({
                 this.templatePath("_layout.jade"),
                 this.destinationPath("src/_layout.jade"),
                 {
-                    appname: "Nanoscope"
+                    appname: _.startCase(this.appname)
                 }
             );
         }
